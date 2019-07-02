0c7ea32c9b793247ebd33519b3ff0d22bb7acd8c
gioung <ska2253@naver.com>
2019-07-02 14:46:00 +0900

    update settings.py

--- python_ch3/settings.py
+++ python_ch3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$se$e7(-01poj8f!4rtsx=7urp8u8v81**htup6k-i1&=6&8pl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
